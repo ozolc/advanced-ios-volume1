86e4183ac49d601985727b5944828c886aacc6eb
Maksim Nosov <ozolc@yandex.ru>
2019-05-30 16:46:16 +0300

    Permissions! Permissions everywhere!

--- HappyDays/HappyDays/MemoriesViewController.swift
+++ HappyDays/HappyDays/MemoriesViewController.swift
@@ -7,24 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
 
 class MemoriesViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        checkPermissions()
     }
-    */
-
+    
+    func checkPermissions() {
+        // check status for all three permissions
+        let photosAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
+        let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission == .granted
+        let transcribeAuthorized = SFSpeechRecognizer.authorizationStatus() == .authorized
+        
+        // make a single boolean out of all three
+        let authorized = photosAuthorized && recordingAuthorized && transcribeAuthorized
+        
+        // if we're missing one, show the first run screen
+        if authorized == false {
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "FirstRun") {
+                navigationController?.present(vc, animated: true)
+            }
+        }
+    }
+    
 }
