b2202d25a86675f084137a571c02cd6bd36e55d5
Maksim Nosov <ozolc@yandex.ru>
2019-06-10 11:37:23 +0300

    Hooking it all up

---
+++ TimeShare/TimeShare MessagesExtension/EventViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventViewController.swift
+//  TimeShare MessagesExtension
+//
+//  Created by Maksim Nosov on 10/06/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveSelectedDates(_ sender: UIButton) {
+    }
+    
+    @IBAction func addDate(_ sender: UIButton) {
+    }
+
+}
