adbcf7c35d5e4bcc248063a39e501e69ba6089f6
Maksim Nosov <ozolc@yandex.ru>
2019-06-11 16:35:10 +0300

    Adding Siri support

--- ChooseCruise/Extension/IntentHandler.swift
+++ ChooseCruise/Extension/IntentHandler.swift
@@ -25,7 +25,42 @@ class IntentHandler: INExtension, INRidesharingDomainHandling {
     }
     
     func handle(intent: INRequestRideIntent, completion: @escaping (INRequestRideIntentResponse) -> Void) {
+        let result = INRequestRideIntentResponse(code: .success, userActivity: nil)
         
+        let status = INRideStatus()
+        
+        // this is our internal value that identifies the ride uniquely in our back-end system
+        status.rideIdentifier = "abc123"
+        
+        // give it the pick up and drop off location we already agreed with the user
+        status.pickupLocation = intent.pickupLocation
+        status.dropOffLocation = intent.dropOffLocation
+        
+        // mark it as confirmed – we will deliver a ride
+        status.phase = INRidePhase.confirmed
+        
+        // say we'll be there in 15 minutes
+        status.estimatedPickupDate = Date(timeIntervalSinceNow: 900)
+        
+        // create a new vehicle and configure it fully
+        let vehicle = INRideVehicle()
+        
+        // workaround: load the car image into UIImage, convert that into PNG data, then create an INImage out of that
+        let image = UIImage(named: "car")!
+        let data = image.pngData()!
+        vehicle.mapAnnotationImage = INImage(imageData: data)
+        
+        // set the vehicle's current location to wherever the user wants to go – this ought to at least place it a little way away for testing purposes
+        vehicle.location = intent.dropOffLocation!.location
+        
+        // now that we have configured the vehicle fully, assign it all at once to status.vehicle
+        status.vehicle = vehicle
+        
+        // attach our finished INRideStatus object to the result
+        result.rideStatus = status
+        
+        // and send it back
+        completion(result)
     }
     
     func handle(intent: INGetRideStatusIntent, completion: @escaping (INGetRideStatusIntentResponse) -> Void) {
@@ -52,9 +87,29 @@ class IntentHandler: INExtension, INRidesharingDomainHandling {
     }
     
     func resolvePickupLocation(for intent: INRequestRideIntent, with completion: @escaping (INPlacemarkResolutionResult) -> Void) {
+        let result: INPlacemarkResolutionResult
+        
+        if let requestedLocation = intent.pickupLocation {
+            // we have a valid pick up location – return success!
+            result = INPlacemarkResolutionResult.success(with: requestedLocation)
+        } else {
+            // no pick up location yet; mark this as outstanding
+            result = INPlacemarkResolutionResult.needsValue()
+        }
+        
+        completion(result)
     }
     
     func resolveDropOffLocation(for intent: INRequestRideIntent, with completion: @escaping (INPlacemarkResolutionResult) -> Void) {
+        let result: INPlacemarkResolutionResult
+        
+        if let requestedLocation = intent.dropOffLocation {
+            result = INPlacemarkResolutionResult.success(with: requestedLocation)
+        } else {
+            result = INPlacemarkResolutionResult.needsValue()
+        }
+        
+        completion(result)
     }
     
     
