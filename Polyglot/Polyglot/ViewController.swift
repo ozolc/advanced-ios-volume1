5007b2ac45c9a18e5951ced07e1814199ca0cb58
Maksim Nosov <ozolc@yandex.ru>
2019-06-18 10:47:53 +0300

    Loading example words

--- Polyglot/Polyglot/ViewController.swift
+++ Polyglot/Polyglot/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var words = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let titleAttributes = [NSAttributedString.Key.font: UIFont(name: "AmericanTypewriter", size: 22)!]
+        navigationController?.navigationBar.titleTextAttributes = titleAttributes
+        title = "POLYGOT"
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedWords = defaults.object(forKey: "Words") as? [String] {
+            words = savedWords
+        } else {
+            saveInitialValues(to: defaults)
+        }
+    }
+    
+    func saveInitialValues(to defaults: UserDefaults) {
+        words.append("bear::l'ours")
+        words.append("camel::le chameau")
+        words.append("cow::la vache")
+        words.append("fox::le renard")
+        words.append("goat::la chèvre")
+        words.append("monkey::le singe")
+        words.append("pig::le cochon")
+        words.append("rabbit::le lapin")
+        words.append("sheep::le mouton")
+        
+        defaults.set(words, forKey: "Words")
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        let word = words[indexPath.row]
+        let split = word.components(separatedBy: "::")
+        
+        cell.textLabel?.text = split[0]
+        
+        return cell
+    }
 
 }
 
