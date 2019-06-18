f5f2229b825e59b537df5c021583b86a766c5dcf
Maksim Nosov <ozolc@yandex.ru>
2019-06-18 10:36:15 +0300

    Polyglot. Setting up

---
+++ Polyglot/Polyglot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Polyglot
+//
+//  Created by Maksim Nosov on 18/06/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
