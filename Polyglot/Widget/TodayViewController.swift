6719ee4b469340ea14544a14a4ea481a5926c8c0
Maksim Nosov <ozolc@yandex.ru>
2019-06-18 11:22:36 +0300

    Getting the extension up and running

--- Polyglot/Widget/TodayViewController.swift
+++ Polyglot/Widget/TodayViewController.swift
@@ -9,13 +9,54 @@
 import UIKit
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
+class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDataSource, UITableViewDelegate {
         
     @IBOutlet weak var tableView: UITableView!
+    
+    var words = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let defaults = UserDefaults.standard
+        if let savedWords = defaults.object(forKey: "Words") as? [String] {
+            words = savedWords
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        let word = words[indexPath.row]
+        let split = word.components(separatedBy: "::")
+        
+        cell.textLabel?.text = split[0]
+        
+        cell.detailTextLabel?.text = ""
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.detailTextLabel?.text == "" {
+                let word = words[indexPath.row]
+                let split = word.components(separatedBy: "::")
+                cell.detailTextLabel?.text = split[1]
+            } else {
+                cell.detailTextLabel?.text = ""
+            }
+        }
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
