388fe6d166899790ddf94db4e2c33965a7968bda
Maksim Nosov <ozolc@yandex.ru>
2019-06-19 12:35:25 +0300

    Editing groups with segues

--- Alarmadillo/Alarmadillo/ViewController.swift
+++ Alarmadillo/Alarmadillo/ViewController.swift
@@ -21,6 +21,40 @@ class ViewController: UITableViewController {
         
         groups.append(Group(name: "Enabled group", playSound: true, enabled: true, alarms: []))
         groups.append(Group(name: "Disabled group", playSound: true, enabled: false, alarms: []))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addGroup))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Groups", style: .plain, target: nil, action: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    @objc func addGroup() {
+        let newGroup = Group(name: "Name this group", playSound: true, enabled: false, alarms: [])
+        groups.append(newGroup)
+        
+        performSegue(withIdentifier: "EditGroup", sender: newGroup)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let groupToEdit: Group
+        
+        if sender is Group {
+            // we were called from addGroup(); use what it sent us
+            groupToEdit = sender as! Group
+        } else {
+            // we were called by a table view cell; figure out which group we're attached to send send that
+            guard let selectedIndexPath = tableView.indexPathForSelectedRow else { return }
+            groupToEdit = groups[selectedIndexPath.row]
+        }
+        
+        // unwrap our destination from the segue
+        if let groupViewController = segue.destination as? GroupViewController {
+            // give it whatever group we decided above
+            groupViewController.group = groupToEdit
+        }
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
